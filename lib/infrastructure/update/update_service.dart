1c0923278d821a5244067165b331bdf0f069aeae
straculencuandrei <andrei.straculencu@ulbsibiu.ro>
2026-09-24 04:40:11 +0300

    Release v0.8.5 (v0.8.5+20)

--- lib/infrastructure/update/update_service.dart
+++ lib/infrastructure/update/update_service.dart
@@ -5,8 +5,8 @@ import 'package:package_info_plus/package_info_plus.dart';
 import 'models/app_update_info.dart';
 
 class UpdateService {
-  static const String defaultFallbackVersion = '0.8.2';
-  static const int defaultFallbackBuildNumber = 19;
+  static const String defaultFallbackVersion = '0.8.5';
+  static const int defaultFallbackBuildNumber = 20;
 
   static String _currentVersion = defaultFallbackVersion;
   static int _currentBuildNumber = defaultFallbackBuildNumber;
@@ -96,6 +96,7 @@ class UpdateService {
     }
   }
 }
+
 
 
 
